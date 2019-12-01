dafa3353684bc8ab02172106412d604bdac87e0d
Firdausi Aditya <fadityadarmawan@gmail.com>
2019-12-01 17:08:07 +0700

    Integrasi sudah

--- Apps.py
+++ Apps.py
@@ -1,28 +1,34 @@
 import requests
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # city = requests.get('http://localhost:5010/kota/' + 'bandung').json()
+def home():    
+    return render_template('home.html')
+
+@app.route('/about')
+def about():    
+    return render_template('about.html')
+
+@app.route('/transportasi', methods=['GET'])
+def transportasi():
+    asalkota = request.args.get('asal')
+    tujuankota = request.args.get('tujuan')
+    
+    kota = requests.get('http://localhost:5010/kota/' + asalkota)
+    transport = requests.get('http://localhost:5020/' + asalkota + '/' + tujuankota)
+
+    data = json.loads(kota.text)
+    dataa = json.loads(transport.text)
+    
+    return render_template('transportasi.html', dataa = dataa, data = data)
+# city = requests.get('http://localhost:5010/kota/' + 'bandung').json()
     # transport = requests.get('http://localhost:5020/sekalitrip/' + 'jakarta' + '/' + 'bandung').json()
     # data = []
     # data.append(city)
     # data.append(transport)
-    # return render_template('home.html', data = data)
-
-@app.route('/kota/<city>', methods=['GET'])
-def kota(city):
-    kota = requests.get('http://localhost:5010/kota/' + city)
-    data = []
-    data.append(kota)
-    return render_template('page.html', data = data)
-
-@app.route('/<asal>/<tujuan>')
-def info_transport(asal, tujuan):
-    transport = requests.get('http://localhost:5020/sekalitrip/' + asal + '/' + tujuan)
-    return transport
 
 if __name__ == '__main__':
 	app.run(debug = True)
